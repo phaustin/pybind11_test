06149756111082755dc2d2cb76654b68391d78d6
Phil Austin <paustin@eos.ubc.ca>
2018-03-03 11:32:48 -0800

    remove numpy

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 export FC=$(which $FC)
 export CC=$(which $CC)
 export CXX=$(which $CXX)
-unset MACOSX_DEPLOYMENT_TARGET
+#unset MACOSX_DEPLOYMENT_TARGET
 mkdir -p build
 rm -rf build/*
 ${PYTHON} setup.py install -v --single-version-externally-managed --record record.txt
